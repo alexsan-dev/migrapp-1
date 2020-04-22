8d9434f0260152ceb310ecadc8260a7b3d15c071
alexsan-dev <as62971@gmail.com>
2020-04-22 15:57:35 -0600

    Updates

---
+++ precache-manifest.0fe6c7cea5cc86cebc7717b8c3d8329a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0482e37ca196d533423a804b55216c",
+    "url": "/migrapp-1/index.html"
+  },
+  {
+    "revision": "ea525ee0ccb6c7bb9f20",
+    "url": "/migrapp-1/static/css/main.0d93896c.chunk.css"
+  },
+  {
+    "revision": "c0a46ff453109f266c9e",
+    "url": "/migrapp-1/static/js/2.8a545648.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/migrapp-1/static/js/2.8a545648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea525ee0ccb6c7bb9f20",
+    "url": "/migrapp-1/static/js/main.5ba5082c.chunk.js"
+  },
+  {
+    "revision": "6a9c583cb1335e0b40bd",
+    "url": "/migrapp-1/static/js/runtime-main.973bf87e.js"
+  }
+]);
